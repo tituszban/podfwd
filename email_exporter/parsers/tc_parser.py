f5505a3e121166cf803fa96376457efcbe064d07
Titusz Ban <tituszban@gmail.com>
2021-01-24 13:19:20 +0000

    Annotate reason section was removed

--- email_exporter/parsers/tc_parser.py
+++ email_exporter/parsers/tc_parser.py
@@ -4,6 +4,7 @@ from ssml_builder.core import Speech
 import bleach
 from bs4 import NavigableString
 
+
 class TcParser(ParserABC):
     def __init__(self, logger):
         self._logger = logger
@@ -18,7 +19,7 @@ class TcParser(ParserABC):
                     accum = []
                 accum.append(c)
             yield accum
-        
+
         split_tags = ["h1", "h2", "h3", "h4"]
 
         for tag in split_tags:
@@ -45,9 +46,72 @@ class TcParser(ParserABC):
             return [text]
 
         sub_sections = self._split_long_content(content)
-        
+
         return [part for section in sub_sections for part in self._to_ssml(section)]
 
+    def _is_relevant(self, content):
+        title_separator = "•"
+        if any(title_separator in c.text for c in content):
+            return False, "has separator"
+        if any(c.name == "img" for c in content):
+            return False, "has img"
+        if any(c.name == "h3" and "sponsored" in c.text.lower() for c in content):
+            return False, "sponsored"
+        call_to_action = [
+            "read more stories",
+            "see more jobs",
+            "privacy policy"
+        ]
+        if any(any(phrase in c.text.lower() for phrase in call_to_action) for c in content):
+            return False, "cta"
+        ignored_sections = [
+            "across the week",
+            "around techcrunch",
+            "equitypod",
+        ]
+        if any(any(phrase in c.text.lower() for phrase in ignored_sections) and "h" in c.name for c in content):
+            return False, "ignored section"
+        return True, ""
+
+    def _decompose_content(self, td):
+        for child in td:
+            if child == "\n":
+                continue
+
+            if child.name == "ul":
+                for li in child.find_all("li"):
+                    yield ContentItem.to_item(li)
+            else:
+                if any(isinstance(c, NavigableString) and c.strip() != '' for c in child.children) or not any(c.name == "p" for c in child.children):
+                    yield ContentItem.to_item(child)
+                else:
+                    for c in child.children:
+                        if isinstance(c, NavigableString) and c.strip() == "":
+                            continue
+                        yield ContentItem.to_item(c)
+
+    def _remove_lines(self, content):
+        def remove_line(line):
+            if "read more" in line.text.lower() and line.is_only_link:
+                return True
+            if "membership program" in line.text.lower():
+                return True
+            if line == "":
+                return True
+            return False
+        return [c for c in content if not remove_line(c)]
+
+    def _to_description(self, content):
+        removed_attributes = ["class", "id", "name", "style"]
+        components = []
+        for c in content:
+            components.append(bleach.clean(
+                str(c.component),
+                attributes=["href", "target", "alt", "src"],
+                tags=bleach.sanitizer.ALLOWED_TAGS +
+                ["a", "img", "p", "h1", "h2", "h3", "h4", "span", "br"]
+            ))
+        return '\n'.join(components)
 
     def parse(self, soup=None, title="", **kwargs):
         assert soup is not None, "Soup not provided"
@@ -61,87 +125,25 @@ class TcParser(ParserABC):
 
         tds = list(find_tds_with_p(table))
 
-        def decompose_content(td):
-            for child in td:
-                if child == "\n":
-                    continue
-
-                if child.name == "ul":
-                    for li in child.find_all("li"):
-                        yield ContentItem.to_item(li)
-                else:
-                    if any(isinstance(c, NavigableString) and c.strip() != '' for c in child.children) or not any(c.name == "p" for c in child.children):
-                        yield ContentItem.to_item(child)
-                    else:
-                        for c in child.children:
-                            if isinstance(c, NavigableString) and c.strip() == "":
-                                continue
-                            yield ContentItem.to_item(c)
-
-        def is_relevant(content):
-            title_separator = "•"
-            if any(title_separator in c.text for c in content):
-                return False
-            if any(c.name == "img" for c in content):
-                return False
-            if any(c.name == "h3" and "sponsored" in c.text.lower() for c in content):
-                return False
-            call_to_action = [
-                "read more stories",
-                "see more jobs",
-                "privacy policy"
-            ]
-            if any(any(phrase in c.text.lower() for phrase in call_to_action) for c in content):
-                return False
-            ignored_sections = [
-                "across the week",
-                "around techcrunch",
-                "equitypod",
-            ]
-            if any(any(phrase in c.text.lower() for phrase in ignored_sections) and "h" in c.name for c in content):
-                return False
-            return True
-
-        def remove_lines(content):
-            def remove_line(line):
-                if "read more" in line.text.lower() and line.is_only_link:
-                    return True
-                if "membership program" in line.text.lower():
-                    return True
-                if line == "":
-                    return True
-                return False
-            return [c for c in content if not remove_line(c)]
-
-        
-
-        def clean_component(content):
-            removed_attributes = ["class", "id", "name", "style"]
-            components = []
-            for c in content:
-                components.append(bleach.clean(
-                    str(c.component),
-                    attributes=["href", "target", "alt", "src"],
-                    tags=bleach.sanitizer.ALLOWED_TAGS + ["a", "img", "p", "h1", "h2", "h3", "h4", "span", "br"]
-                ))
-            return '\n'.join(components)
-
         def select_voice():
             if "startups weekly" in title.lower():
                 return "en-US-Wavenet-H"
-            return None # use default
+            return None  # use default
 
         ssmls = []
         descriptions = []
+        ignored = []
 
         for td in tds:
-            content = list(decompose_content(td))
-            if not is_relevant(content):
+            content = list(self._decompose_content(td))
+            relevant, reason = self._is_relevant(content)
+            if not relevant:
+                ignored.append((content, reason))
                 continue
-            content = remove_lines(content)
+            content = self._remove_lines(content)
 
             ssmls += self._to_ssml(content)
-            desc = clean_component(content)
+            desc = self._to_description(content)
             descriptions.append(desc)
 
         return ssmls, descriptions, select_voice()
