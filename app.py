6d837118793ba05961c9ebadb7f56c57bc87d64a
Titusz Ban <tituszban@gmail.com>
2021-09-05 12:34:02 +0100

    Only allow POST methods to invoke exporter

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def index():
+    return "Email exporter must be invoked with POST"
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def handle_request():
     return export_inbox()
-
 
 @app.route("/fingerprint")
 def get_fingerprint():
