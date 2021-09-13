646263bcc44ad24ec35cd7099490dec9a7e63705
Titusz Ban <tituszban@gmail.com>
2021-09-13 09:21:27 +0100

    Improve gmail inbox selection

--- email_exporter/inbox/gmail_inbox.py
+++ email_exporter/inbox/gmail_inbox.py
@@ -1,5 +1,6 @@
 import imaplib
 import email
+import re
 from .inbox_abc import InboxABC
 
 
@@ -27,16 +28,47 @@ class GmailInbox(InboxABC):
 
         _, data = self._mail.uid('STORE', idx, '+FLAGS', '(\\FLAGGED)')
 
+    def _ensure_success(self, action_result, success="OK"):
+        status, result = action_result
+        if status != success:
+            raise Exception(f"Inbox returned not OK status: {status}; {action_result}")
+        return result
+
+    def _get_all_mail_folder(self):
+        result = self._ensure_success(self._mail.list())
+        
+        result_list = list(map(lambda b: b.decode(), result))
+        inbox_folder_re = re.compile(r"^\((?P<tags>(\\\w*\s?)*)\)\s\"/\"\s(?P<name>\".*\")$")
+        folder_matches = list(map(inbox_folder_re.match, result_list))
+        folders = list(map(
+            lambda m: (
+                tuple(map(lambda tag: tag.lstrip("\\").lower(), m.group("tags").split())),
+                m.group("name")),
+            folder_matches))
+        
+        all_folder = list(filter(lambda folder: "all" in folder[0], folders))
+
+        if len(all_folder) <= 0:
+            raise Exception("No folder found with 'All' tag")
+        if len(all_folder) > 1:
+            raise Exception("More than one folder found with 'All' tag")
+
+        return all_folder[0][1]
+
+
+
     def get_messages(self):
         if self._mail is None:
             self._login()
 
-        self._mail.select('"[Gmail]/All mail"')
+        all_folder = self._get_all_mail_folder()
 
-        _, ids = self._mail.uid('search', None, 'UNFLAGGED')
+        self._ensure_success(self._mail.select(all_folder))
+
+        ids = self._ensure_success(self._mail.uid('search', None, 'UNFLAGGED'))
         ids = ids[0].decode().split()
         for idx in ids:
-            _, messageRaw = self._mail.uid('fetch', idx, '(RFC822)')
+            messageRaw = self._ensure_success(self._mail.uid('fetch', idx, '(RFC822)'))
             message = email.message_from_bytes(messageRaw[0][1])
 
             yield idx, message
