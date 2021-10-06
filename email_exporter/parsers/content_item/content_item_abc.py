a55cf0518dca15291bdbb62bfb3c5a71524cd38c
Titusz Ban <tituszban@gmail.com>
2021-10-06 18:12:25 +0100

    Add repr to content items

--- email_exporter/parsers/content_item/content_item_abc.py
+++ email_exporter/parsers/content_item/content_item_abc.py
@@ -10,8 +10,7 @@ class ContentItemABC(ABC):
 
     @abstractmethod
     def get_ssml(self):
-        return []
-        # raise NotImplementedError()
+        raise NotImplementedError()
 
     @abstractmethod
     def get_description(self):
@@ -24,3 +23,13 @@ class ContentItemABC(ABC):
 
     def _get_text_content(self):
         return get_text_content(self._component)
+
+    def _get_repr_name(self):
+        module_path = type(self).__module__.split(".")
+        if "content_item" not in module_path:
+            return f"{'.'.join(module_path[:-1])}.{type(self).__name__}"
+
+        return f"{'.'.join(module_path[module_path.index('content_item'):-1])}.{type(self).__name__}"
+
+    def __repr__(self):
+        return f"{self._get_repr_name()}({self._get_text_content()})"
