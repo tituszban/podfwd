b65469f1a988a5437d39651fb345be9110331056
Titusz Ban <tituszban@gmail.com>
2021-10-25 09:30:18 +0100

    Create feed endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,5 @@ from .fingerprinting import *       # noqa: F403, F401, E402
 
 if os.environ.get("ENABLE_EXPORTER"):
     from .exporter import *       # noqa: F403, F401
+if os.environ.get("ENABLE_FEED"):
+    from .feed import *       # noqa: F403, F401
---
+++ app/feed.py
@@ -0,0 +1,23 @@
+import os
+from flask import redirect, Response
+from . import app
+
+from email_exporter.feed_endpoint import get_feed_rss
+
+DEFAULT_REDIRECT = "https://www.podfwd.com"
+
+
+@app.route("/")
+def redirect_index():
+    redirect_to = os.environ.get("REDIRECT_TO", DEFAULT_REDIRECT)
+    return redirect(redirect_to)
+
+
+@app.route("/<feed_name>", methods=["GET"])
+def get_feed_name(feed_name):
+    try:
+        rss = get_feed_rss(feed_name)
+    except KeyError:
+        return "Feed not found", 404
+
+    return Response(rss, mimetype="text/xml")
