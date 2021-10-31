1e6879b4aa9e10fabb9123b07ad72c63e9db0ee3
Titusz Ban <tituszban@gmail.com>
2021-10-31 12:10:48 +0000

    Inbox processor type hints

--- email_exporter/inbox/inbox_processor.py
+++ email_exporter/inbox/inbox_processor.py
@@ -6,20 +6,24 @@ import re
 import email
 from bs4 import BeautifulSoup
 from .inbox_item import InboxItem
-from typing import Callable, Tuple
+from typing import Callable, Iterator, Tuple, Union
 
 
 class InboxProcessor:
     def __init__(self, config: Config, logger: Logger, inbox: Inbox):
+        self._config = config
         self._logger = logger
         self._inbox = inbox
 
-    def _decode_header(self, header):
+    def _decode_header(self, header: str) -> str:
         default_charset = "utf-8"
         dh = email.header.decode_header(header)
-        return ''.join(t.decode(e or default_charset) if type(t) == bytes else t for t, e in dh)
+        return ''.join(
+            str(t.decode(encoding or default_charset)) if type(t) == bytes else str(t)
+            for t, encoding in dh
+        )
 
-    def _remove_fwd(self, subject):
+    def _remove_fwd(self, subject: str) -> str:
         prefixes = ["Fwd: ", "FW: "]
         for prefix in prefixes:
             if subject.startswith(prefix):
@@ -39,7 +43,7 @@ class InboxProcessor:
 
         return subject, sender, recipient, date
 
-    def _identify_participants(self, sender, recipient, mime):
+    def _identify_participants(self, sender: str, recipient: str, mime: str) -> Tuple[str, Union[str, None]]:
         if recipient != self._inbox.email_address:
             return (recipient, sender)
 
@@ -48,20 +52,20 @@ class InboxProcessor:
 
         return (sender, None)
 
-    def _get_payload(self, message: Message):
+    def _get_payload(self, message: Message) -> Iterator[bytes]:
         if message.is_multipart():
             for m in message.get_payload():
                 yield from self._get_payload(m)
         else:
             yield message.get_payload(decode=True)
 
-    def _to_soup(self, raw):
+    def _to_soup(self, raw: str) -> Union[BeautifulSoup, None]:
         soup = BeautifulSoup(raw, 'html.parser')
         if soup.find("p") is None:
             return None
         return soup
 
-    def _try_decode(self, payload):
+    def _try_decode(self, payload: bytes) -> Tuple[str, str]:
         encodings = ("utf-8", "windows-1252")
         errors = []
         for encoding in encodings:
@@ -69,7 +73,7 @@ class InboxProcessor:
                 return payload.decode(encoding), encoding
             except UnicodeDecodeError as e:
                 errors.append(str(e))
-        raise UnicodeDecodeError(
+        raise RuntimeError(
             "None of the known encodings were able to decode this payload; {}".format('; '.join(errors)))
 
     def _process_email(self, message: Message) -> InboxItem:
